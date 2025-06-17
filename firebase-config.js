7ca3b113e5dfbf0f7e5b8de6069a7fb2676a6776
Logscrj <logisticascrj@gmail.com>
2025-06-16 22:55:06 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Configuração Firebase - SUBSTITUA pela sua configuração
+const firebaseConfig = {
+  apiKey: "AIzaSyBW97PFcSJIWhBKkItSZNzVCfpCIE5BkPk",
+  authDomain: "rastreamento-etex-2025.firebaseapp.com",
+  databaseURL: "https://rastreamento-etex-2025-default-rtdb.firebaseio.com",
+  projectId: "rastreamento-etex-2025",
+  storageBucket: "rastreamento-etex-2025.firebasestorage.app",
+  messagingSenderId: "607585690829",
+  appId: "1:607585690829:web:323475e162ffc364283584"
+};
+
+// Exportar configuração
+window.firebaseConfig = firebaseConfig;
